be5d440ee3027e26e1fe22210cedd3ac01c3a11b
Wellinson Silva <wellinsonsilva064@gmail.com>
2025-09-11 13:13:22 +0000

    Try fixing this error: `Console Error: Unknown event handler property `o

--- src/components/ui/toaster.tsx
+++ src/components/ui/toaster.tsx
@@ -1,3 +1,4 @@
+
 "use client"
 
 import { useToast } from "@/hooks/use-toast"
@@ -16,8 +17,10 @@ export function Toaster() {
   return (
     <ToastProvider>
       {toasts.map(function ({ id, title, description, action, ...props }) {
+        // Apenas para evitar passar props desconhecidas para o componente Toast
+        const { onDismiss, onAutoClose, ...toastProps } = props;
         return (
-          <Toast key={id} {...props}>
+          <Toast key={id} {...toastProps}>
             <div className="grid gap-1">
               {title && <ToastTitle>{title}</ToastTitle>}
               {description && (
